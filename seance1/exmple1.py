95b049bd967c490b5dd18f6202698fe4168f8413
nermin ouesleti <nerminouesleti99@gmail.com>
2024-02-01 14:45:38 +0100

    exmple1 : variable , types de base, print

---
+++ seance1/exmple1.py
@@ -0,0 +1,11 @@
+nom='Nermin'# var type str
+age=24 #var type int 
+salaire =5200 #float
+est_celib=True #booléen
+#afficher info de la personne 
+#on utilise print
+print(nom)
+print(salaire)
+print('l\'employe"',nom,"\"a comme salaire", salaire,
+      "et comme age",age,"celibataire:",est_celib, sep=",")
+
